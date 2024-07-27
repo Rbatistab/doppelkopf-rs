f7e1287380c2fa7b623df358a0a8ecc207cb74da
Rbtistab <russbatisbarrrco@gmail.com>
2024-07-27 17:44:58 -0600

    Refactor into 2 crates (cards and game)

---
+++ game-cards/src/lib/card/card.rs
@@ -0,0 +1,27 @@
+// enum StandardCardRank {
+//     TWO,
+//     THREE,
+//     FOUR,
+//     FIVE,
+//     SIX,
+//     SEVEN,
+//     EIGHT,
+//     NINE,
+//     TEN,
+//     JACK,
+//     QUEEN,
+//     KING,
+//     ACE,
+// }
+//
+// enum Suit {
+//     SPADES,
+//     HEARTS,
+//     DIAMONDS,
+//     CUBS,
+// }
+//
+// struct Card {
+//     rank: StandardCardRank,
+//     symbol: Suit,
+// }
